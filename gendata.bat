585284e2f87c7fbfe8b69b3a0bb1f2a6c97acd55
Eugene <fncombo@gmail.com>
2018-01-18 17:27:25 +0000

    commit

---
+++ gendata.bat
@@ -0,0 +1,55 @@
+@echo off
+    setlocal enabledelayedexpansion
+
+    REM get all folders in the current folder, echo their names and size in bytes
+
+    echo var localRawData ^= ^[ > data.js
+
+    set "folder=G:\Anime\Series"
+    for /d %%a in ("%folder%\*") do (
+        set "size=0"
+        for /f "tokens=3,5" %%b in ('dir /-c /a /w /s "%%~fa\*" 2^>nul ^| findstr /b /c:"  "') do if "%%~c"=="" set "size=%%~b"
+        echo "%%~nxa#!size!"^, >> data.js
+    )
+
+    set "folder=G:\Anime\Movies"
+    for %%a in ("%folder%\*") do (
+        echo "%%~nxa#%%~za"^, >> data.js
+    )
+
+    set "folder=G:\Anime\Ghibli Movies"
+    for %%a in ("%folder%\*") do (
+        echo "%%~nxa#%%~za"^, >> data.js
+    )
+
+    set "folder=G:\Anime\Special and OVA"
+    for %%a in ("%folder%\*") do (
+        echo "%%~nxa#%%~za"^, >> data.js
+    )
+
+    echo ^]; >> data.js
+
+    endlocal
+
+    REM get my anime list page and echo contents
+
+    echo var malHTML ^= ^` >> data.js
+    powershell -Command "Invoke-WebRequest https://myanimelist.net/animelist/fncombo -OutFile malhtml.html"
+    copy data.js+malhtml.html data.js
+    echo ^`; >> data.js
+
+    REM get my anime list api and echo contents
+
+    echo var malXML ^= ^` >> data.js
+    powershell -Command "Invoke-WebRequest https://myanimelist.net/malappinfo.php?u=fncombo\"^&\"status=all\"^&\"type=anime -OutFile malxml.html"
+    copy data.js+malxml.html data.js
+    echo ^`; >> data.js
+
+    REM delete temp files
+
+    del "malhtml.html"
+    del "malxml.html"
+
+    REM last updated date and time
+
+    echo var batchUpdated ^= "%date% %time%" >> data.js
