e08158d256b22eaa3effcfe30d7676af7d152f43
Eugene <fncombo@gmail.com>
2019-09-26 12:34:10 +0100

    Generate episode duration from cache data

--- data/helpers.js
+++ data/helpers.js
@@ -18,8 +18,23 @@ function getRewatchCount(tags) {
     return match ? parseInt(match[1], 10) : 0
 }
 
+/**
+ * Extract the total duration in minutes from API data.
+ */
+function getDuration(duration) {
+    // Convert hours into minutes
+    if (/hr/i.test(duration)) {
+        const [ hours, minutes ] = duration.match(/(\d+)\s?hr\.?\s?(\d+)?/i)
+
+        return (parseInt(hours, 10) * 60) + (minutes ? parseInt(minutes, 10) : 0)
+    }
+
+    return parseInt(duration, 10)
+}
+
 // Exports
 module.exports = {
     removeInvalidChars,
     getRewatchCount,
+    getDuration,
 }
