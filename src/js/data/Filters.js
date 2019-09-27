1b6bd79e28c2bfb932fc3b9d0f947223dd77e349
Eugene <fncombo@gmail.com>
2019-09-27 10:34:36 +0100

    Comments

--- src/js/data/Filters.js
+++ src/js/data/Filters.js
@@ -4,10 +4,16 @@ import React from 'react'
 // Helpers
 import Icon from 'js/helpers/Icon'
 
+/**
+ * Function to reverse sort filter values.
+ */
 function reverseSort(values) {
     return values.sort((a, b) => b - a)
 }
 
+/**
+ * Function to exclude falsy filter values.
+ */
 function excludeBlankValues(values) {
     return values.filter(value => !!value)
 }
