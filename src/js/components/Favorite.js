6b3650e59dd42e65b9461cef96da9222650a03a8
Eugene <fncombo@gmail.com>
2019-10-10 14:55:08 +0100

    Update Favorite.js

--- src/js/components/Favorite.js
+++ src/js/components/Favorite.js
@@ -18,7 +18,7 @@ function Favorite({ number }) {
             // eslint-disable-next-line max-len
             title={`Favourite anime #${number}.\n\nThis refers to the whole series in general, or a specific season of that series.\n\nThese don't have to be rated 10 because they are special regardless in their own way.`}
         >
-            <span class="favorite-number">{number}</span>
+            <span className="favorite-number">{number}</span>
         </Icon>
     )
 }
