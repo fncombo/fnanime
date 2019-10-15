9400a0f9c310eed9f86978359facb5faaa3a8582
Eugene <fncombo@gmail.com>
2019-10-15 12:10:54 +0100

    Separate search input into a separate component

--- src/js/components/filters/Filters.js
+++ src/js/components/filters/Filters.js
@@ -1,5 +1,5 @@
 // React
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 
 // Style
 import 'scss/Filters.scss'
@@ -10,6 +10,7 @@ import { FILTERS } from 'js/data/Filters'
 
 // Components
 import FilterButtonGroup from 'js/components/filters/FilterButtonGroup'
+import Search from 'js/components/filters/Search'
 import Select from 'js/components/filters/Select'
 import Summary from 'js/components/filters/Summary'
 
@@ -18,24 +19,9 @@ import Summary from 'js/components/filters/Summary'
  */
 function Filters() {
     const { state: { anime }, dispatch } = useContext(GlobalState)
-    const [ searchValue, setSearchValue ] = useState('')
-
-    // Callback for the search input change
-    const search = ({ target: { value: searchQuery } }) => {
-        // Update the input value
-        setSearchValue(searchQuery)
-
-        // Search the anime list
-        dispatch({
-            type: ACTIONS.SEARCH,
-            searchQuery,
-        })
-    }
 
     // Reset filters, sorting, and search
     const reset = () => {
-        setSearchValue('')
-
         dispatch({ type: ACTIONS.RESET })
     }
 
@@ -53,13 +39,7 @@ function Filters() {
                 <FilterButtonGroup filterName="source" />
                 <FilterButtonGroup filterName="audioCodec" />
                 <div className="column is-6-mobile is-3-tablet">
-                    <input
-                        type="text"
-                        className="input"
-                        placeholder="Search"
-                        value={searchValue}
-                        onChange={search}
-                    />
+                    <Search />
                 </div>
                 <div className="column is-6-mobile is-2-tablet">
                     <Select filterName="subs" />
---
+++ src/js/components/filters/Search.js
@@ -0,0 +1,28 @@
+// React
+import React, { useContext } from 'react'
+
+// Style
+import 'scss/Filters.scss'
+
+// Data
+import { GlobalState, ACTIONS } from 'js/data/GlobalState'
+
+/**
+ * Search input to filter anime by a query string.
+ */
+function Search() {
+    const { state: { searchQuery }, dispatch } = useContext(GlobalState)
+
+    // Callback for the search input change
+    const search = ({ target: { value } }) => {
+        dispatch({
+            type: ACTIONS.SEARCH,
+            searchQuery: value,
+        })
+    }
+
+    return <input type="text" className="input" placeholder="Search" value={searchQuery} onChange={search} />
+}
+
+// Exports
+export default Search
