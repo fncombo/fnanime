56a592028a5ecaa4c5e1a42af4a8a6fd81ec52f8
Eugene <fncombo@gmail.com>
2019-10-16 14:56:20 +0100

    Add comment

--- src/js/components/StuckSentinel.js
+++ src/js/components/StuckSentinel.js
@@ -4,6 +4,10 @@ import React from 'react'
 // Libraries
 import { useInView } from 'react-intersection-observer'
 
+/**
+ * Creates a sentinel element which tracks whether the content is stuck based on whether the sentinel element
+ * is on screen or not. Relies on CSS to correctly position the sentinel element and other elements.
+ */
 function StuckSentinel({ className, children }) {
     const [ ref, inView, entry ] = useInView()
 
