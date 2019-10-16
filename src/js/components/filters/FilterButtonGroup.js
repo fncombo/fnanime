f0595c1e562bd4d7d460be3f05868ecb98eab92e
Eugene <fncombo@gmail.com>
2019-10-16 09:53:17 +0100

    Re-arrange classes

--- src/js/components/filters/FilterButtonGroup.js
+++ src/js/components/filters/FilterButtonGroup.js
@@ -19,7 +19,7 @@ import FilterButton from 'js/components/filters/FilterButton'
 function FilterButtonGroup({ filterName, isFullWidth }) {
     // The button group is either half wide or takes up the whole row
     const classes = classNames(
-        'column is-flex buttons has-addons is-12-mobile ',
+        'column buttons has-addons is-flex is-12-mobile',
         isFullWidth ? 'is-12-tablet' : 'is-6-tablet'
     )
 
