6b23dff4429f8a9eae1f687d8d31bf074585da44
Eugene <fncombo@gmail.com>
2019-10-24 11:03:19 +0100

    Improve API update message state changes

--- src/js/components/UpdateMessage.js
+++ src/js/components/UpdateMessage.js
@@ -1,5 +1,5 @@
 // React
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 
 // Libraries
 import classNames from 'classnames'
@@ -13,17 +13,32 @@ import Icon from 'js/components/Icon'
 /**
  * Message indicating the loading status of the API update.
  */
-function UpdateMessage() {
+function UpdateMessage({ hideTimeout = 3000 }) {
     const { state: { apiUpdated, apiError } } = useContext(GlobalState)
+    const [ isHidden, setIsHidden ] = useState(false)
 
-    const classes = classNames('message', apiUpdated ? 'is-done' : 'is-loading')
+    const classes = classNames('message', {
+        'is-loading': !apiUpdated,
+        'is-hidden': isHidden,
+    })
 
     let updateStatusMessage
 
+    // If the API has been updated
     if (apiUpdated) {
+        // Message based on success or error
         updateStatusMessage = apiError
             ? <><Icon icon="times-circle" /> Error contacting API</>
             : <><Icon icon="check-circle" /> Updated</>
+
+        // Set timeout to hide the message
+        if (!isHidden) {
+            setTimeout(() => {
+                setIsHidden(true)
+            }, hideTimeout)
+        }
+
+    // Loading in progress message
     } else {
         updateStatusMessage = <><Icon icon="database" /> Loading latest information</>
     }
