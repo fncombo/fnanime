cf4f19f16392bfd122f4551f84a69e7af4c78ee2
Eugene <fncombo@gmail.com>
2019-11-01 12:01:30 +0000

    No need for class names function

--- src/js/components/filters/FilterButtonGroup.js
+++ src/js/components/filters/FilterButtonGroup.js
@@ -1,9 +1,6 @@
 // React
 import React from 'react'
 
-// Libraries
-import classNames from 'classnames'
-
 // Style
 import 'scss/Filters.scss'
 
@@ -18,10 +15,7 @@ import FilterButton from 'js/components/filters/FilterButton'
  */
 function FilterButtonGroup({ filterName, isFullWidth }) {
     // The button group is either half wide or takes up the whole row
-    const classes = classNames(
-        'column buttons has-addons is-flex is-12-mobile',
-        isFullWidth ? 'is-12-tablet' : 'is-6-tablet'
-    )
+    const classes = `column buttons has-addons is-flex is-12-mobile ${isFullWidth ? 'is-12-tablet' : 'is-6-tablet'}`
 
     return (
         <div className={classes}>
