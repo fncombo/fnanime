b67cca8f2828556d13958ad4ce010025450d212e
Eugene <fncombo@gmail.com>
2020-08-04 20:40:47 +0100

    Remove husky arg

--- data/generate.js
+++ data/generate.js
@@ -465,7 +465,7 @@ getApiData(MAL_USERNAME)
         // Commit and push data if the "commit" argument was passed
         console.log('Committing data to repository')
 
-        await git().commit('Anime data JSON update', ANIME_JSON_LOCATION, { '--no-verify': null })
+        await git().commit('Anime data JSON update', ANIME_JSON_LOCATION)
 
         console.log('Pushing master to origin')
 
