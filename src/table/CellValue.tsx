ec0abd95aa78904c5327fcc96d927db8537f6fcf
Eugene <fncombo@gmail.com>
2021-03-08 14:46:24 +0000

    rename component

--- src/table/CellValue.tsx
+++ src/table/CellValue.tsx
@@ -4,10 +4,10 @@ import { FilterName, FilterValue } from '../filters/config'
 import FilterTag from '../filters/FilterTag'
 
 /**
- * Displays data for a table cell. If the cell is empty, a dash is shown. If a filter name is given, the value will
+ * Displays the value of a table cell. If the cell is empty, a dash is shown. If a filter name is given, the value will
  * be rendered as a filter tag.
  */
-const TableCell: FunctionComponent<{
+const CellValue: FunctionComponent<{
     filter?: FilterName
     children: JSX.Element
 }> = ({ filter, children }) => {
@@ -22,4 +22,4 @@ const TableCell: FunctionComponent<{
     return children
 }
 
-export default TableCell
+export default CellValue
